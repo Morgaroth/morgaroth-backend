8d324955f64d3e30f0926c897f314eb88cd212b8
Morgaroth <mateuszjaje@gmail.com>
2017-05-07 15:09:52 +0200

    refactored implementation of loading managers to based on reflection

--- app/src/main/scala/io/github/morgaroth/app/App.scala
+++ app/src/main/scala/io/github/morgaroth/app/App.scala
@@ -3,21 +3,34 @@ package io.github.morgaroth.app
 import akka.actor.ActorSystem
 import com.typesafe.config.ConfigFactory
 import io.github.morgaroth.base.ServiceManager
+import org.reflections.Reflections
 
 import scala.collection.JavaConverters._
 import scala.reflect._
 
 object App {
-
-  def companionObj[T](name: String)(implicit man: Manifest[T]) = {
-    val c = Class.forName(name + "$")
-    c.getField("MODULE$").get(c).asInstanceOf[ServiceManager]
+  def main(args: Array[String]): Unit = {
+    alternativeMain()
   }
 
-  def main(args: Array[String]): Unit = {
+  private def baseMain() {
+    def companionObj[T](name: String)(implicit man: Manifest[T]) = {
+      val c = Class.forName(name + "$")
+      c.getField("MODULE$").get(c).asInstanceOf[ServiceManager]
+    }
+
     val system = ActorSystem("morgaroth")
     ConfigFactory.load().getStringList("managers").asScala.toList.foreach { className =>
       companionObj(className).initialize(system)
+    }
+  }
+
+  def alternativeMain() {
+    val system = ActorSystem("morgaroth")
+    val reflection = new Reflections("io.github.morgaroth")
+    val implementations = reflection.getSubTypesOf(classOf[ServiceManager]).asScala
+    implementations.foreach { clazz =>
+      clazz.getField("MODULE$").get(clazz).asInstanceOf[ServiceManager].initialize(system)
     }
   }
 }
