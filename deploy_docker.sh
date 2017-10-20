2776fca9c96a1d8e0162eb8f64d40236af1a30be
Morgaroth <mateuszjaje@gmail.com>
2017-10-20 16:38:34 +0200

    refactored docker scripts

--- deploy_docker.sh
+++ deploy_docker.sh
@@ -1,35 +1,37 @@
 #!/usr/bin/env bash
 
 
-CONTAINER_ID=morgarothserver
+CONTAINER_NAME=morgarothserver
 VERSION=$1
 echo "Deploying version $VERSION"
-A=`docker inspect -f {{.State.Running}} ${CONTAINER_ID}`
-B=`docker inspect -f {{.State}} ${CONTAINER_ID}`
+A=`docker inspect -f {{.State.Running}} ${CONTAINER_NAME}`
+B=`docker inspect -f {{.State}} ${CONTAINER_NAME}`
 echo "'$A' '$B' '$?'"
 if [ "$A" = "true" ]; then
-    echo "Docker $CONTAINER_ID is running, killing them..."
-    docker kill ${CONTAINER_ID}
+    echo "Docker $CONTAINER_NAME is running, killing them..."
+    docker kill ${CONTAINER_NAME}
 else
-    echo "Docker $CONTAINER_ID not found."
+    echo "Docker $CONTAINER_NAME not found."
 fi
 sleep 3
 if [ "$B" != "" ]; then
-    echo "Docker $CONTAINER_ID exists, removing them..."
-    docker rm ${CONTAINER_ID}
+    echo "Docker $CONTAINER_NAME exists, removing them..."
+    docker rm ${CONTAINER_NAME}
 else
-    echo "Docker $CONTAINER_ID not found."
+    echo "Docker $CONTAINER_NAME not found."
 fi
-#            -e pass="$1" \
-docker run -d \
+
+docker run --detach \
             --restart=always \
-            --name ${CONTAINER_ID} \
-            -u `stat -c "%u:%g" .` \
-            -e KOKPIT_BOT_API_KEY \
-            -e GPBETTINGLEAGUE_USERNAME \
-            -e GPBETTINGLEAGUE_PASSWORD \
-            -e "MONGODB_URI=mongodb://172.17.0.1:28017" \
-            -e "REMOTE_SELENIUM_SERVER=http://172.17.0.1:4444/wd/hub" \
+            --name ${CONTAINER_NAME} \
+            --user `stat -c "%u:%g" .` \
+            --env KOKPIT_BOT_API_KEY \
+            --env GPBETTINGLEAGUE_USERNAME \
+            --env GPBETTINGLEAGUE_PASSWORD \
+            --env "MONGODB_URI=mongodb://172.17.0.1:28017" \
+            --env "REMOTE_SELENIUM_SERVER=http://172.17.0.1:4444/wd/hub" \
+            --publish 8888:8080 \
+            --expose 8888 \
             morgarothserver:${VERSION}
 
 echo "Docker fired!"
